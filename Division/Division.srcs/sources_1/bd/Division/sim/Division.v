8d04699791dcc7df8d366df4437c562e599040a0
SnrNotHere16 <aquineda16@hotmail.com>
2021-08-23 16:20:18 -0700

    Revert "Floating point test without 3 fp modules"
    This reverts commit dc96c9c502867b4dab2f1cd840696251028b021b.

--- Division/Division.srcs/sources_1/bd/Division/sim/Division.v
+++ Division/Division.srcs/sources_1/bd/Division/sim/Division.v
@@ -1,7 +1,7 @@
 //Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
 //--------------------------------------------------------------------------------
 //Tool Version: Vivado v.2019.1 (win64) Build 2552052 Fri May 24 14:49:42 MDT 2019
-//Date        : Mon Aug 23 15:46:29 2021
+//Date        : Sun Aug 22 14:25:19 2021
 //Host        : DESKTOP-NDOLUA7 running 64-bit major release  (build 9200)
 //Command     : generate_target Division.bd
 //Design      : Division
@@ -9,7 +9,7 @@
 //--------------------------------------------------------------------------------
 `timescale 1 ps / 1 ps
 
-(* CORE_GENERATION_INFO = "Division,IP_Integrator,{x_ipVendor=xilinx.com,x_ipLibrary=BlockDiagram,x_ipName=Division,x_ipVersion=1.00.a,x_ipLanguage=VERILOG,numBlks=2,numReposBlks=2,numNonXlnxBlks=0,numHierBlks=0,maxHierDepth=0,numSysgenBlks=0,numHlsBlks=0,numHdlrefBlks=1,numPkgbdBlks=0,bdsource=USER,da_axi4_cnt=20,da_board_cnt=6,da_mb_cnt=6,synth_mode=OOC_per_IP}" *) (* HW_HANDOFF = "Division.hwdef" *) 
+(* CORE_GENERATION_INFO = "Division,IP_Integrator,{x_ipVendor=xilinx.com,x_ipLibrary=BlockDiagram,x_ipName=Division,x_ipVersion=1.00.a,x_ipLanguage=VERILOG,numBlks=4,numReposBlks=4,numNonXlnxBlks=0,numHierBlks=0,maxHierDepth=0,numSysgenBlks=0,numHlsBlks=0,numHdlrefBlks=1,numPkgbdBlks=0,bdsource=USER,da_axi4_cnt=20,da_board_cnt=6,da_mb_cnt=6,synth_mode=OOC_per_IP}" *) (* HW_HANDOFF = "Division.hwdef" *) 
 module Division
    (LED,
     a,
@@ -47,6 +47,8 @@ module Division
   wire DivisionTop_0_g;
   wire clk_1;
   wire [31:0]floating_point_0_m_axis_result_tdata;
+  wire [31:0]floating_point_1_m_axis_result_tdata;
+  wire [31:0]floating_point_2_m_axis_result_tdata;
   wire reset_1;
 
   assign LED[15:0] = DivisionTop_0_LED;
@@ -79,8 +81,20 @@ module Division
        (.aclk(clk_1),
         .m_axis_result_tdata(floating_point_0_m_axis_result_tdata),
         .m_axis_result_tready(1'b1),
-        .s_axis_a_tdata(DivisionTop_0_F1),
+        .s_axis_a_tdata(floating_point_1_m_axis_result_tdata),
         .s_axis_a_tvalid(1'b0),
-        .s_axis_b_tdata(DivisionTop_0_F2),
+        .s_axis_b_tdata(floating_point_2_m_axis_result_tdata),
         .s_axis_b_tvalid(1'b0));
+  Division_floating_point_1_0 floating_point_1
+       (.aclk(clk_1),
+        .m_axis_result_tdata(floating_point_1_m_axis_result_tdata),
+        .m_axis_result_tready(1'b1),
+        .s_axis_a_tdata(DivisionTop_0_F1),
+        .s_axis_a_tvalid(1'b0));
+  Division_floating_point_2_0 floating_point_2
+       (.aclk(clk_1),
+        .m_axis_result_tdata(floating_point_2_m_axis_result_tdata),
+        .m_axis_result_tready(1'b1),
+        .s_axis_a_tdata(DivisionTop_0_F2),
+        .s_axis_a_tvalid(1'b0));
 endmodule
