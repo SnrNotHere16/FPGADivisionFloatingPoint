b150f3eb9bdbcbcd423d6c3ccbba2fb654b43b0a
SnrNotHere16 <aquineda16@hotmail.com>
2021-08-30 12:13:42 -0700

    File management

--- FloatingPointDivisionNexys4DDR/Division/Division.sdk/.sdk/launch_scripts/xilinx_c-c++_application_(system_debugger)/system_debugger_using_debug_division1.elf_on_local.tcl
+++
@@ -1,8 +0,0 @@
-connect -url tcp:127.0.0.1:3121
-configparams mdm-detect-bscan-mask 2
-targets -set -nocase -filter {name =~ "microblaze*#0" && bscan=="USER2"  && jtag_cable_name =~ "Digilent Nexys4DDR 210292A8AD8EA"} -index 0
-rst -processor
-targets -set -nocase -filter {name =~ "microblaze*#0" && bscan=="USER2"  && jtag_cable_name =~ "Digilent Nexys4DDR 210292A8AD8EA"} -index 0
-dow C:/Users/OAkun/Division/Division.sdk/Division1/Debug/Division1.elf
-targets -set -nocase -filter {name =~ "microblaze*#0" && bscan=="USER2"  && jtag_cable_name =~ "Digilent Nexys4DDR 210292A8AD8EA"} -index 0
-con
