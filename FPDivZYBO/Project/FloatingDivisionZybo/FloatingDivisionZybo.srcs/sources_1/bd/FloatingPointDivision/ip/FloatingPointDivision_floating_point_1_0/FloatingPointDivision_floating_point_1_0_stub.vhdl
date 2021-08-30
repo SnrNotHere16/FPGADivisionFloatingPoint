c417971d8c9a965019ff91e4ada023e7ad8b2880
SnrNotHere16 <aquineda16@hotmail.com>
2021-08-30 12:58:45 -0700

    a

---
+++ FPDivZYBO/Project/FloatingDivisionZybo/FloatingDivisionZybo.srcs/sources_1/bd/FloatingPointDivision/ip/FloatingPointDivision_floating_point_1_0/FloatingPointDivision_floating_point_1_0_stub.vhdl
@@ -0,0 +1,36 @@
+-- Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2019.1 (win64) Build 2552052 Fri May 24 14:49:42 MDT 2019
+-- Date        : Wed Aug 25 20:04:22 2021
+-- Host        : DESKTOP-NDOLUA7 running 64-bit major release  (build 9200)
+-- Command     : write_vhdl -force -mode synth_stub
+--               c:/Users/OAkun/FloatingDivisionZybo/FloatingDivisionZybo.srcs/sources_1/bd/FloatingPointDivision/ip/FloatingPointDivision_floating_point_1_0/FloatingPointDivision_floating_point_1_0_stub.vhdl
+-- Design      : FloatingPointDivision_floating_point_1_0
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7z020clg400-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity FloatingPointDivision_floating_point_1_0 is
+  Port ( 
+    aclk : in STD_LOGIC;
+    s_axis_a_tvalid : in STD_LOGIC;
+    s_axis_a_tready : out STD_LOGIC;
+    s_axis_a_tdata : in STD_LOGIC_VECTOR ( 31 downto 0 );
+    m_axis_result_tvalid : out STD_LOGIC;
+    m_axis_result_tready : in STD_LOGIC;
+    m_axis_result_tdata : out STD_LOGIC_VECTOR ( 31 downto 0 )
+  );
+
+end FloatingPointDivision_floating_point_1_0;
+
+architecture stub of FloatingPointDivision_floating_point_1_0 is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "aclk,s_axis_a_tvalid,s_axis_a_tready,s_axis_a_tdata[31:0],m_axis_result_tvalid,m_axis_result_tready,m_axis_result_tdata[31:0]";
+attribute x_core_info : string;
+attribute x_core_info of stub : architecture is "floating_point_v7_1_8,Vivado 2019.1";
+begin
+end;
